ce784f8a893c3fb0ee6958eb28250f81815583a8
Kevin Spinar <spinarkm@gmail.com>
2020-12-09 18:35:19 -0500

    add LIPH_COMPILES macro utility

---
+++ compiles/compiles.hpp
@@ -0,0 +1,30 @@
+#ifndef LIPH_COMPILES_HPP
+#define LIPH_COMPILES_HPP
+
+#include <type_traits>
+#include <utility>
+
+#define LIPH_WRAP(...) __VA_ARGS__
+#define LIPH_ARG(x) (liph_wrapper, (x))
+#define LIPH_DECLVAL(type) (liph_wrapper, std::declval<type>())
+
+#define LIPH_COMPILES(type, expr) ((liph::details::make_overloaded( \
+                [](long, auto) constexpr { return false; }, \
+                [](int, auto *liph_wrapper, std::void_t<decltype(sizeof(*liph_wrapper) && (expr))>* = 0) constexpr { return true; } \
+        )) \
+        (0, static_cast<liph::details::wrapper<type>*>(nullptr)))
+
+
+namespace liph {
+namespace details {
+
+template<typename... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+
+template<typename... Ts> constexpr auto make_overloaded(Ts... funcs) { return overloaded<Ts...> { funcs... }; }
+
+template<typename... Ts> struct wrapper {};
+
+}
+}
+
+#endif
---
+++ compiles/example.cpp
@@ -0,0 +1,107 @@
+#include "compiles.hpp"
+#include <iostream>
+#include <tuple>
+
+
+template<typename T>
+void test1(const T &x) {
+    // LIPH_ARG needs to wrap the template parameters
+    if constexpr(LIPH_COMPILES(T, LIPH_ARG(x) / 5)) {
+        std::cout << "can divide\n";
+    } else {
+        std::cout << "no / operator\n";
+    }
+}
+
+template<typename T>
+void test2() {
+    // if you don't have an object of the template type
+    if constexpr(LIPH_COMPILES(T, *LIPH_DECLVAL(T))) {
+        std::cout << "dereferenceable\n";
+    } else {
+        std::cout << "no dereference operator\n";
+    }
+}
+
+template<typename T, typename U>
+void test3(const T &x, const U &y) {
+    // only one of the template arguments needs to be provided.
+    // I picked T; U could have been used instead.
+    // it doesn't matter which one; it just needs to be one of arguments to
+    // the function itself (ie, not a "fixed template argument")
+    if constexpr(LIPH_COMPILES(T, LIPH_ARG(x) + y)) {  
+        std::cout << "can add\n";
+    } else {
+        std::cout << "no + operator\n";
+    }
+}
+
+
+template<typename T>
+class foo {
+public:
+    // don't need this below trick to delay evaluation of the template parameter
+    // template<typename U = T>
+    void test4() {
+        if constexpr(LIPH_COMPILES(T, std::cout << LIPH_ARG(x))) {
+            std::cout << "foo::x has stream output\n";
+        } else {
+            std::cout << "no << operator\n";
+        }
+    }
+
+    T x;
+};
+
+
+template<typename... Args>
+void test5(Args&&... args) {
+    // parameter packs can be used
+    if constexpr(LIPH_COMPILES(Args..., ((std::cout << LIPH_ARG(args)), ...))) {
+        std::cout << "each argument has <<\n";
+    } else {
+        std::cout << "not all arguments have <<\n";
+    }
+}
+
+
+template<typename... Args>
+void test6(const std::tuple<Args...> &) {
+    // parameter packs can be used
+    if constexpr(LIPH_COMPILES(Args..., ((std::cout << LIPH_DECLVAL(Args)), ...))) {
+        std::cout << "each element in the tuple has <<\n";
+    } else {
+        std::cout << "not all elements in tuple have <<\n";
+    }
+}
+
+
+// works in c++20 (but then I'm not sure why you wouldn't just use concepts)
+//template<typename T>
+//std::enable_if_t<LIPH_COMPILES(T, LIPH_DECLVAL(T) / 5)> bar() {}
+
+
+struct test_struct {};
+
+
+int main() {
+    test1(3);
+    test1("foo");
+    test2<int>();
+    test2<int*>();
+    test3(5, 2.3);
+    test3("foo", "bar");
+    
+    foo<int> f;
+    f.test4();
+
+    foo<test_struct> f2;
+    f2.test4();
+
+    test5(2, 12.0, "test");
+    test5(2, test_struct());
+
+    test6(std::tuple<int, double, const char*>{2, 12.0, "test"});
+    test6(std::tuple<int, test_struct>());
+    //bar<int>();
+}
