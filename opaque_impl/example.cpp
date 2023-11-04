2ef88edd3720917a8f81fddf682989e15781d1d9
Kevin Spinar <spinarkm@gmail.com>
2023-11-04 16:29:39 -0500

    fix tabs vs spaces

--- opaque_impl/example.cpp
+++ opaque_impl/example.cpp
@@ -3,10 +3,10 @@
 
 int main() {
     rect r(3, 5);
-	 rect s(r);
+    rect s(r);
     std::cout << r.area() << ' ' << s.area() << std::endl;
     r.set_width(4);
     std::cout << r.area() << ' ' << s.area() << std::endl;
-	 s = r;
-	 std::cout << r.area() << ' ' << s.area() << std::endl;
+    s = r;
+    std::cout << r.area() << ' ' << s.area() << std::endl;
 }
